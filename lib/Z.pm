8b0a718ea0f74c4a5c5562f37ace29de8a51f105
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-18 23:15:14 +0100

    extra imports

--- lib/Z.pm
+++ lib/Z.pm
@@ -16,8 +16,10 @@ BEGIN {
 }
 
 sub import {
-	my ($target, $class, $mode) = ( scalar caller, shift, @_ );
-	$mode = '-modern' unless defined $mode;
+	my ($target, $class ) = ( scalar caller, shift );
+	
+	my $mode = '-modern';
+	$mode = shift if $_[0] =~ /^-/;
 	
 	my $collection = 'modules';
 	
@@ -29,11 +31,11 @@ sub import {
 		
 		if ( $mode eq '-modern' ) {
 			require Carp;
-			return Carp::croak("$target requires Perl v5.14 or above; stopping");
+			return Carp::croak( "$target requires Perl v5.14 or above; stopping" );
 		}
 		elsif ( $mode eq '-detect' ) {
 			require Carp;
-			Carp::carp("$target may require Perl v5.14 or above; attempting compatibility mode");
+			Carp::carp( "$target may require Perl v5.14 or above; attempting compatibility mode" );
 		}
 	}
 	
@@ -46,6 +48,8 @@ sub import {
 		require indirect;
 		'indirect'->unimport::out_of( $target );
 	};
+	
+	$class->also( $target, @_ );
 	
 	use_module( 'namespace::autoclean' )->import::into( $target );
 	
@@ -89,6 +93,137 @@ sub compat_modules {
 	}
 	
 	return @modules;
+}
+
+my %also = (
+	Dumper => sub {
+		require Data::Dumper;
+		return sub {
+			local $Data::Dumper::Deparse;
+			Data::Dumper::Dumper(@_);
+		},
+	},
+	croak => sub {
+		return sub {
+			require Carp;
+			Carp::croak( @_ > 1 ? sprintf(shift, @_) : @_ );
+		};
+	},
+	carp => sub {
+		return sub {
+			require Carp;
+			Carp::carp( @_ > 1 ? sprintf(shift, @_) : @_ );
+		};
+	},
+	cluck => sub {
+		return sub {
+			require Carp;
+			Carp::cluck( @_ > 1 ? sprintf(shift, @_) : @_ );
+		};
+	},
+	maybe => sub {
+		if ( eval 'use PerlX::Maybe::XS 0.003 (); 1' ) {
+			return \&PerlX::Maybe::XS::maybe;
+		}
+		return sub ($$@) {
+			( defined $_[0] and defined $_[1] )
+				? @_
+				: ( ( @_ > 1 ) ? @_[2 .. $#_] : qw() )
+		};
+	},
+	provided => sub {
+		if ( eval 'use PerlX::Maybe::XS 0.003 (); 1' ) {
+			return \&PerlX::Maybe::XS::provided;
+		}
+		return sub ($$$@) {
+			( shift )
+				? @_
+				: ( ( @_ > 1 ) ? @_[2 .. $#_] : qw() )
+		};
+	},
+	all            => q(List::Util),
+	any            => q(List::Util),
+	first          => q(List::Util),
+	head           => q(List::Util),
+	max            => q(List::Util),
+	maxstr         => q(List::Util),
+	min            => q(List::Util),
+	minstr         => q(List::Util),
+	none           => q(List::Util),
+	notall         => q(List::Util),
+	pairfirst      => q(List::Util),
+	pairgrep       => q(List::Util),
+	pairkeys       => q(List::Util),
+	pairmap        => q(List::Util),
+	pairs          => q(List::Util),
+	pairvalues     => q(List::Util),
+	product        => q(List::Util),
+	reduce         => q(List::Util),
+	reductions     => q(List::Util),
+	sample         => q(List::Util),
+	shuffle        => q(List::Util),
+	sum            => q(List::Util),
+	sum0           => q(List::Util),
+	tail           => q(List::Util),
+	uniq           => q(List::Util),
+	uniqnum        => q(List::Util),
+	uniqstr        => q(List::Util),
+	unpairs        => q(List::Util),
+	blessed        => q(Scalar::Util),
+	dualvar        => q(Scalar::Util),
+	isdual         => q(Scalar::Util),
+	isvstring      => q(Scalar::Util),
+	isweak         => q(Scalar::Util),
+	looks_like_number => q(Scalar::Util),
+	openhandle     => q(Scalar::Util),
+	readonly       => q(Scalar::Util),
+	refaddr        => q(Scalar::Util),
+	reftype        => q(Scalar::Util),
+	set_prototype  => q(Scalar::Util),
+	tainted        => q(Scalar::Util),
+	unweaken       => q(Scalar::Util),
+	weaken         => q(Scalar::Util),
+	prototype      => q(Sub::Util),
+	set_prototype  => q(Sub::Util),
+	set_subname    => q(Sub::Util),
+	subname        => q(Sub::Util),
+);
+
+sub also {
+	my ( $class, $target ) = ( shift, shift );
+	
+	my %imports;
+	for my $arg ( @_ ) {
+		my ( $func, $dest ) = split /:/, $arg;
+		$dest = $func unless $dest;
+		
+		my $source = $also{$func} or do {
+			require Carp;
+			Carp::croak( "Do not know where to find function $func" );
+			next;
+		};
+		
+		push @{ $imports{ ref($source) or $source } ||= [] },
+			ref($source) ? [ $dest, $source ] : [ $dest, $func ];
+	}
+	
+	for my $source ( sort keys %imports ) {
+		if ( $source eq 'CODE' ) {
+			for my $func ( @{$imports{$source}} ) {
+				my ( $name, $gen ) = @$func;
+				no strict 'refs';
+				*{"$target\::$name"} = $gen->();
+			}
+		}
+		else {
+			use_module( $source );
+			for my $func ( @{$imports{$source}} ) {
+				my ( $name, $orig ) = @$func;
+				no strict 'refs';
+				*{"$target\::$name"} = \&{"$source\::$orig"};
+			}
+		}
+	}
 }
 
 1;
@@ -161,6 +296,32 @@ modules:
  # Prints a warning and uses compatible modules on Perl 5.8.8+.
 
 The default is C<< -modern >>.
+
+=head2 Additional Functions
+
+There are a whole bunch of other useful functions that Z I<could> make
+available, but it's hard to know the best place to draw the line. So
+other functions are available on request:
+
+ use Z qw( weaken unweaken isweak );
+ 
+ use Z -compat, qw( pairmap pairgrep );
+ 
+ # Rename functions...
+ use Z qw( pairmap:pmap pairgrep:pgrep );
+
+(The things listed in the L</SYNOPSIS> are always imported and don't
+support the renaming feature.)
+
+The additional functions available are: everything from L<Scalar::Util>,
+everything from L<List::Util>, everything from L<Sub::Util>, everything
+from L<Carp> (wrapped versions with C<sprintf> functionality, except
+C<confess> which is part of the standard set of functions already),
+C<Dumper> from L<Data::Dumper>, and C<maybe> and C<provided> from
+L<PerlX::Maybe>.
+
+If you specify a compatibility mode (like C<< -modern >>), this must be
+first in the import list.
 
 =head1 BUGS
 
